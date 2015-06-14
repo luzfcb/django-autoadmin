d580e91178db3973a9ac391389782728d5c8fecb
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-06-14 03:43:38 -0400

    Bumping version to appease pypi's: "This filename has previously been used, you should use a different version."

--- autoadmin/__init__.py
+++ autoadmin/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
 
 __author__ = 'Roberto Rosario'
-__build__ = 0x010000
+__build__ = 0x010001
 __copyright__ = 'Copyright 2014 Roberto Rosario'
 __license__ = 'MIT'
 __title__ = 'django-autoadmin'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 default_app_config = 'autoadmin.apps.AutoAdminAppConfig'
